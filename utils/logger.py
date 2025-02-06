a7fae8e9748c678f4e68ba06d66a7adf230192f6
Emilio Luna <lunaemilio2003@gmail.com>
2025-02-06 02:43:35 -0300

    corregido problema con el antiguo instalador

--- utils/logger.py
+++ utils/logger.py
@@ -1,30 +1,20 @@
-import logging
 import os
+import logging
+from datetime import datetime
 
+appdata_path = os.getenv('APPDATA')
+logs_dir = os.path.join(appdata_path, 'SePaGen', 'logs')
 
-'''The function creates a log file in a 'logs' directory and logs messages with timestamp.
-    
-Parameters
-----------
-report
-    The code you provided sets up a logging system in Python that creates a log file in a 'logs'
-directory within the current directory. The `log()` function is used to log reports to this file
-with an INFO level.
-'''
-current_directory = os.path.dirname(os.path.abspath(__file__))
+os.makedirs(logs_dir, exist_ok=True)
 
-log_directory = os.path.join(current_directory, 'logs')
-
-os.makedirs(log_directory, exist_ok=True)
-
-log_file_path = os.path.join(log_directory, 'logs.log')
+log_file = os.path.join(logs_dir, 'logs.log')
 
 logging.basicConfig(
-    filename=log_file_path,
-    filemode='a',
+    filename=log_file,
     level=logging.INFO,
-    format="%(asctime)s - %(message)s",
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S'
 )
 
-def log(report):
-    logging.info(report)
+def log(message: str):
+    logging.info(message)
